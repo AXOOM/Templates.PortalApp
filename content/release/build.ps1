c3546f31753559dbbbeb46ed19428d9fffc23215
Bastian Eicher <bastian.eicher@axoom.com>
2017-12-01 19:48:08 +0100

    Tweaked release build script

--- content/release/build.ps1
+++ content/release/build.ps1
@@ -3,6 +3,6 @@ $ErrorActionPreference = "Stop"
 Push-Location $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 $dockerRegistry = $(if ($Version.Contains("-")) {"docker-ci.axoom.cloud"} else {"docker.axoom.cloud"})
-ax release asset.yml $Version --arg DOCKER_REGISTRY=$dockerRegistry --arg PORTAL_APP=@app.json --arg IDENTITY_CLIENT=@client.json
+ax release --verbose --refresh asset.yml $Version --arg DOCKER_REGISTRY=$dockerRegistry --arg PORTAL_APP=@app.json --arg IDENTITY_CLIENT=@client.json
 
 Pop-Location
