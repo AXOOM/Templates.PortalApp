137397e7865f4c1ca91d551e6dd38d77907fe8ff
Bastian Eicher <bastian.eicher@axoom.com>
2017-12-20 12:14:56 +0100

    Simplified Startup

--- content/src/Axoom.MyApp/Startup.cs
+++ content/src/Axoom.MyApp/Startup.cs
@@ -16,8 +16,6 @@ namespace Axoom.MyApp
     [UsedImplicitly]
     public class Startup
     {
-        public IHostingEnvironment Environment { get; }
-
         public IConfigurationRoot Configuration { get; }
 
         /// <summary>
@@ -25,7 +23,6 @@ namespace Axoom.MyApp
         /// </summary>
         public Startup(IHostingEnvironment env)
         {
-            Environment = env;
             Configuration = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddYamlFile("appsettings.yml", optional: false, reloadOnChange: true)
@@ -51,14 +48,14 @@ namespace Axoom.MyApp
         /// Called by ASP.NET Core to configure services after they have been registered.
         /// </summary>
         [UsedImplicitly]
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IServiceProvider provider)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider provider)
         {
             loggerFactory
                 .AddAxoomConsole(Configuration.GetSection("Logging"))
                 .CreateLogger<Startup>()
                 .LogInformation("Starting My App");
 
-            app.UseRestApi(Environment);
+            app.UseRestApi(env);
         }
     }
 }
