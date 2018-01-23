99b875cf8d9ff289c9d934d8834eedd5cfd9ad00
Bastian Eicher <bastian.eicher@axoom.com>
2018-01-23 13:14:50 +0100

    Tweaked Program.cs

--- content/src/Axoom.MyApp/Program.cs
+++ content/src/Axoom.MyApp/Program.cs
@@ -5,7 +5,9 @@ namespace Axoom.MyApp
 {
     public static class Program
     {
-        public static void Main() => new WebHostBuilder()
+        public static void Main(string[] args) => BuildWebHost(args).Run();
+
+        public static IWebHost BuildWebHost(string[] args) => new WebHostBuilder()
             .UseKestrel()
             .UseContentRoot(Directory.GetCurrentDirectory())
             .UseStartup<Startup>()
