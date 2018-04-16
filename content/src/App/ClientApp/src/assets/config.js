bbb729fa04d8dbc0dc690241fe2f14c070871fdd
Md Intekhab Shaukat <mdintekhab.shaukat@axoom.com>
2018-04-16 16:23:32 +0200

    Added config.js to store app config for frontend

---
+++ content/src/App/ClientApp/src/assets/config.js
@@ -0,0 +1,9 @@
+﻿var AXOOM_APP = AXOOM_APP || {};
+AXOOM_APP.CONFIG = {
+    OAUTH: {
+        clientId: 'axoom-myapp',
+        scope: 'openid profile email axoom-myapp.api',
+        // Uncomment below line to activate Authentication in frontend.
+        //identityServerUri: 'http://identity-myinstance.local.myaxoom.eu'
+    }
+};
