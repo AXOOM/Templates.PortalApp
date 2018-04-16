f76302d29b1c7a7cd4a5fe0cb13ab9beff79ede1
Md Intekhab Shaukat <mdintekhab.shaukat@axoom.com>
2018-04-16 16:28:02 +0200

    Added authorization header for api access

--- content/src/App/ClientApp/src/app/contacts/contacts.component.ts
+++ content/src/App/ClientApp/src/app/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-contacts',
@@ -8,19 +9,33 @@ import { HttpClient } from '@angular/common/http';
 export class ContactsComponent {
   public contacts: ContactDto[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<ContactDto[]>(baseUrl + 'api/contacts/').subscribe(result => {
-        this.contacts = result;
+  constructor(
+    http: HttpClient,
+    private oauthService: OAuthService,
+    @Inject('BASE_URL') baseUrl: string
+  ) {
+    const getOptions = {
+      headers: this.getHeaders()
+    };
+
+    http.get<ContactDto[]>(baseUrl + 'api/contacts/', getOptions).subscribe(result => {
+      this.contacts = result;
     }, error => console.error(error));
+  }
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': 'Bearer ' + this.oauthService.getAccessToken()
+    });
   }
 }
 
 interface ContactDto {
-    id: string;
-    firstName: string;
-    lastName: string;
+  id: string;
+  firstName: string;
+  lastName: string;
 }
 
 interface NoteDto {
-    content: string;
+  content: string;
 }
