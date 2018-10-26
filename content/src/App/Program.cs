2459beaff90f7e75fc1eaacc3bbaf67887bdd391
Bastian Eicher <bastian.eicher@axoom.com>
2018-10-26 11:11:05 +0200

    Moved Logging config to Program

--- content/src/App/Program.cs
+++ content/src/App/Program.cs
@@ -1,26 +1,37 @@
 using System.IO;
+using Axoom.Extensions.Logging.Console;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace MyVendor.MyApp
 {
+    /// <summary>
+    /// Manages process lifetime, configuration and logging.
+    /// </summary>
     public static class Program
     {
         public static void Main()
-            => new WebHostBuilder().UseKestrel()
-                                   .UseContentRoot(Directory.GetCurrentDirectory())
-                                   .ConfigureAppConfiguration(Configuration)
-                                   .UseStartup<Startup>()
-                                   .Build()
-                                   .Run();
-
-        private static void Configuration(WebHostBuilderContext context, IConfigurationBuilder builder)
-        {
-            var env = context.HostingEnvironment;
-            builder.SetBasePath(env.ContentRootPath)
-                  .AddYamlFile("appsettings.yml", optional: false, reloadOnChange: true)
-                  .AddYamlFile($"appsettings.{env.EnvironmentName}.yml", optional: true, reloadOnChange: true)
-                  .AddEnvironmentVariables();
-        }
+            => new WebHostBuilder()
+              .UseKestrel()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .ConfigureAppConfiguration((context, builder) =>
+               {
+                   var env = context.HostingEnvironment;
+                   builder.SetBasePath(env.ContentRootPath)
+                          .AddYamlFile("appsettings.yml", optional: false, reloadOnChange: true)
+                          .AddYamlFile($"appsettings.{env.EnvironmentName}.yml", optional: true, reloadOnChange: true)
+                          .AddEnvironmentVariables();
+               })
+              .ConfigureLogging((context, builder) =>
+               {
+                   var config = context.Configuration.GetSection("Logging");
+                   builder.AddConfiguration(config)
+                          .AddAxoomConsole(config)
+                          .AddExceptionDemystifyer();
+               })
+              .UseStartup<Startup>()
+              .Build()
+              .Run();
     }
 }
