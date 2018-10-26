64216fa5102aee5f6d11fd8e63654112e73e9bc1
Bastian Eicher <bastian.eicher@axoom.com>
2018-10-26 11:11:22 +0200

    Replaced bundled XunitLogger with MartinCostello.Logging.XUnit

--- content/src/UnitTests/Controllers/ControllerTestBase.cs
+++ content/src/UnitTests/Controllers/ControllerTestBase.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MyVendor.MyApp.Infrastructure;
 using Xunit.Abstractions;
 
@@ -15,13 +17,20 @@ namespace MyVendor.MyApp.Controllers
 
         protected readonly HttpClient Client;
 
-        protected ControllerTestBase(ITestOutputHelper output)
+        protected ControllerTestBase(ITestOutputHelper output, IDictionary<string, string> configuration = null)
         {
-            _server = new TestServer(new WebHostBuilder()
-                                    .ConfigureServices(x => x.AddLogging(builder => builder.AddXunit(output))
-                                                             .AddWeb(new ConfigurationBuilder().Build()))
-                                    .ConfigureServices(ConfigureService)
-                                    .Configure(x => x.UseWeb()));
+            _server = new TestServer(
+                new WebHostBuilder()
+                   .ConfigureAppConfiguration(builder =>
+                    {
+                        if (configuration != null)
+                            builder.AddInMemoryCollection(configuration);
+                    })
+                   .ConfigureLogging(builder => builder.AddXUnit(output))
+                   .ConfigureServices((context, services) => services.AddWeb(context.Configuration))
+                   .ConfigureServices(ConfigureService)
+                   .Configure(builder => builder.UseWeb()));
+
             Client = _server.CreateClient();
         }
 
