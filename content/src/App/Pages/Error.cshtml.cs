c269d48ccc13fdc9ece36af8519f9a42e5347fc8
Bastian Eicher <bastian.eicher@axoom.com>
2018-12-14 13:43:41 +0100

    Updated to .NET Core 2.2

--- content/src/App/Pages/Error.cshtml.cs
+++ content/src/App/Pages/Error.cshtml.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyVendor.MyApp.Pages
@@ -14,7 +9,6 @@ namespace MyVendor.MyApp.Pages
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
