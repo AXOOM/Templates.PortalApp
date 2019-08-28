0a5cb7d6cd33535825621f212ccaeafdc0105301
Bastian Eicher <bastian.eicher@axoom.com>
2019-08-28 19:56:44 +0200

    Improved error handling PowerShell scripts

--- build.ps1
+++ build.ps1
@@ -1,9 +1,8 @@
 ﻿Param ([string]$Version = "0.1-dev")
 $ErrorActionPreference = "Stop"
-
 pushd $PSScriptRoot
 
-# Build NuGet Package
 nuget pack -Version $Version -OutputDirectory artifacts -NoPackageAnalysis
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
